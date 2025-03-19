49879fad28672fa5eef1bd137de8035c30a96bf6
Drew Sommer <drew@sommerengineering.com>
2025-03-19 16:32:58 +0100

    feat: Calculate high and low watermarks

--- strat.py
+++ strat.py
@@ -17,5 +17,9 @@ df_prices = yf_ticker = yf.Ticker(TICKER).history(start=START_DATE,end=END_DATE,
 # normalize timestamps
 df_prices.index = df_prices.index.tz_localize(None)
 
-
+# watermarks of SMA over lookback periods
+def high_watermark(highs, lookback):
+    return pd.Series(highs).rolling(lookback).max()
+def low_watermark(lows, lookback):
+    return pd.Series(lows).rolling(lookback).min()
 
