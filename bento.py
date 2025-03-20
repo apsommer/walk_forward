cd3e28e6e1db8f2344b0a428e2e1389b622b557a
Drew Sommer <drew@sommerengineering.com>
2025-03-20 18:06:35 +0100

    feat: Request 1 year of 1m ohlcv

--- bento.py
+++ bento.py
@@ -1,6 +1,6 @@
 import databento as db
 
-client = db.Historical("db-HKrvWfTtEGucr3M6T7EJd5nQmQUyH")
+client = db.Historical("db-aQfdnPCwVsm7qyWRNLkim4nRqp4V4")
 
 # data = client.timeseries.get_range(
 #     dataset="GLBX.MDP3",
@@ -11,33 +11,46 @@ client = db.Historical("db-HKrvWfTtEGucr3M6T7EJd5nQmQUyH")
 # )
 #
 # data.replay(print)
+#
+# def rank_by_volume(top=10):
+#     data = client.timeseries.get_range(
+#         dataset="GLBX.MDP3",
+#         symbols="ALL_SYMBOLS",
+#         schema="ohlcv-1d",
+#         start="2023-08-15",
+#         end="2023-08-16",
+#     )
+#     df = data.to_df()
+#     return df.sort_values(by="volume", ascending=False).instrument_id.tolist()[:top]
+#
+# top_instruments = rank_by_volume()
+# print(top_instruments)
+#
+# def get_symbol_properties(instrument_id_list):
+#     data = client.timeseries.get_range(
+#         dataset="GLBX.MDP3",
+#         stype_in="instrument_id",
+#         symbols=instrument_id_list,
+#         schema="definition",
+#         start="2023-08-15",
+#         end="2023-08-16",
+#     )
+#     return data.to_df()[[
+#         "instrument_id", "raw_symbol",
+#         "min_price_increment", "match_algorithm", "expiration"
+#     ]]
+#
+# print(get_symbol_properties(top_instruments).to_markdown())
 
-def rank_by_volume(top=10):
+def nq():
     data = client.timeseries.get_range(
         dataset="GLBX.MDP3",
-        symbols="ALL_SYMBOLS",
-        schema="ohlcv-1d",
+        symbols=["ES.c.0"],
+        stype_in="continuous",
+        schema="ohlcv-1m",
         start="2023-08-15",
-        end="2023-08-16",
+        end="2024-08-16",
     )
-    df = data.to_df()
-    return df.sort_values(by="volume", ascending=False).instrument_id.tolist()[:top]
+    return data.to_df()
 
-top_instruments = rank_by_volume()
-print(top_instruments)
-
-def get_symbol_properties(instrument_id_list):
-    data = client.timeseries.get_range(
-        dataset="GLBX.MDP3",
-        stype_in="instrument_id",
-        symbols=instrument_id_list,
-        schema="definition",
-        start="2023-08-15",
-        end="2023-08-16",
-    )
-    return data.to_df()[[
-        "instrument_id", "raw_symbol",
-        "min_price_increment", "match_algorithm", "expiration"
-    ]]
-
-print(get_symbol_properties(top_instruments).to_markdown())
+print(nq().to_markdown())
