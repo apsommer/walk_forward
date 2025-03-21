51b37f0f276effe6172f8d363c60f2fd48425019
Drew Sommer <drew@sommerengineering.com>
2025-03-21 11:57:35 +0100

    docs: Add comments

--- DataLayer.py
+++ DataLayer.py
@@ -5,24 +5,25 @@ import local.api_keys as keys
 client = db.Historical(keys.bento_api_key)
 
 def getPrices(
-    starting_date,
-    ending_date):
+    starting_date = "2024-10-15",
+    ending_date = "2025-01-01",
+    schema = "ohlcv-1m"):
 
     df_prices = (client.timeseries.get_range(
         dataset="GLBX.MDP3",
         symbols=["NQ.v.0"],
         stype_in="continuous",
-        schema="ohlcv-1m",
+        schema=schema,
         start=starting_date,
         end=ending_date)
                  .to_df())
 
-    # format names, normalize timestamps
+    # normalize timestamps
     df_prices.index = df_prices.index.tz_localize(None)
-    df_prices.rename(columns={"open": "Open", "high": "High", "low": "Low", "close": "Close"},
-                     inplace=True)
-    df_prices.index.rename("timestamp", inplace=True)
 
-    # remove unwanted columns
-    clean = df_prices[df_prices.columns.drop(['symbol', 'rtype', 'instrument_id', 'publisher_id', 'volume'])]
-    return clean
+    # scrub dataframe
+    df_prices.rename(columns={"open": "Open", "high": "High", "low": "Low", "close": "Close"},inplace=True)
+    df_prices.index.rename("timestamp", inplace=True)
+    clean_df_prices = df_prices[df_prices.columns.drop(['symbol', 'rtype', 'instrument_id', 'publisher_id', 'volume'])]
+
+    return clean_df_prices
--- ExampleBacktester.py
+++ ExampleBacktester.py
@@ -1,22 +1,23 @@
 from backtesting import Backtest
 import DataLayer as dl
 from ExampleStrategy import BuyLowSellHighStrategy
-import pandas as pd
 
 # constants
 initial_cash = 10_000_000_000_000
 starting_date = "2024-10-15"
 ending_date = "2025-01-01"
-filename = "df_prices.xlsx"
+schema = "ohlcv-1h"
 
 df_prices = dl.getPrices(
     starting_date,
-    ending_date)
+    ending_date,
+    schema)
 
 bt = Backtest(df_prices, BuyLowSellHighStrategy, cash=initial_cash, commission=0, exclusive_orders=True)
 stats = bt.run()
 print(stats)
 
+filename = "df_prices.xlsx"
 df_prices.to_excel(filename)
 
-# bt.plot() # todo backtesting.py does not appear able to plot 1m candles?
+bt.plot() # todo backtesting.py does not appear able to plot 1m candles?
