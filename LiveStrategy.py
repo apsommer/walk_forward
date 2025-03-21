3d93848129e1863f09104781d4194a9ed3fea631
Drew Sommer <drew@sommerengineering.com>
2025-03-21 21:33:47 +0100

    docs: Add comments

--- LiveStrategy.py
+++ LiveStrategy.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 from backtesting import Strategy
-from backtesting.test import SMA
 
 class LiveStrategy(Strategy):
 
@@ -20,51 +19,17 @@ class LiveStrategy(Strategy):
     coolOffMinutes = 5
     positionEntryMinutes = 1
 
-    #############################################
-
+    # constants
     fastAngle = fastAngleFactor / 1000.0
     slowAngle = slowAngleFactor / 1000.0
     takeProfit = takeProfitPercent / 100.0
 
     fastCrossover = (fastCrossoverPercent / 100.0) * takeProfit
-    if (takeProfit == 0):
+    if takeProfit == 0:
         fastCrossover = fastCrossoverPercent / 100.0
 
-    def init(self):
-
-        isStrategyEnabled = False
-        strategyStartIndex = 0
-        fastSlope = 0.0
-        slowSlope = 0.0
-        slowPositiveBarIndex = 0
-        slowNegativeBarIndex = 0
-        isFastCrossoverLong = False
-        isFastCrossoverShort = False
-        isEntryDisabled = False
-        isEntryLongDisabled = False
-        isEntryShortDisabled = False
-        hasLongEntryDelayElapsed = False
-        hasShortEntryDelayElapsed = False
-        isEntryLongEnabled = False
-        isEntryShortEnabled = False
-        isEntryLong = False
-        isEntryShort = False
-        isEntryLongPyramid = False
-        isEntryShortPyramid = False
-        isExitLongFastCrossoverEnabled = False
-        isExitShortFastCrossoverEnabled = False
-        isExitLongFastCrossover = False
-        isExitShortFastCrossover = False
-        isExitLongFastMomentum = False
-        isExitShortFastMomentum = False
-        isExitLongTakeProfit = False
-        isExitShortTakeProfit = False
-        isExitLong = False
-        isExitShort = False
-        longExitBarIndex = 0
-        shortExitBarIndex = 0
-        position = 0
-        quantity = 0
+    # todo ?
+    # def init(self):
 
     def next(self):
 
@@ -83,21 +48,21 @@ class LiveStrategy(Strategy):
         fastSlope = np.rad2deg(np.arctan(normalizedFastPrice))
         slowSlope = np.rad2deg(np.arctan(normalizedSlowPrice))
 
+        # price crosses through fast average in favorable direction
         isFastCrossoverLong = ((fastSlope > self.fastAngle and
                                (fast > open or fast > close[1])) and
                                high > fast)
-
         isFastCrossoverShort = ((-self.fastAngle > fastSlope and
                                 (open > fast or close[1] > fast)) and
                                 fast > low)
 
-        # allow entry at start of trend only
+        # allow entry only during starting minutes of trend
         if (np.min(fastSlope, self.disableEntryMinutes) > 0 and self.disableEntryMinutes != 0):
             self.isEntryLongDisabled = True
         if (0 > np.max(fastSlope, self.disableEntryMinutes) and self.disableEntryMinutes != 0):
             self.isEntryShortDisabled = True
 
-        # handle short volatility
+        # handle short volatility todo can remove or simplify?
         if (fast > np.max(open, self.positionEntryMinutes) or self.positionEntryMinutes == 0):
             self.isEntryLongEnabled = True
         if (np.min(open, self.positionEntryMinutes) > fast or self.positionEntryMinutes == 0):
@@ -107,16 +72,24 @@ class LiveStrategy(Strategy):
 
         # todo entry restriction idea
 
+        # entries
         isEntryLong = (self.position == 0
                        and isFastCrossoverLong
                        and not self.isEntryDisabled
                        and not self.isEntryLongDisabled
                        and self.isEntryLongEnabled
                        and slowSlope > self.slowAngle)
-        
         isEntryShort = (self.position == 0
                          and isFastCrossoverShort
                          and not self.isEntryDisabled
                          and not self.isEntryShortDisabled
                          and self.isEntryShortEnabled
                          and -self.slowAngle > slowSlope)
+
+        # exit crossing back into fast in unfavorable direction
+
+        # exit due to excessive momentum in unfavorable direction
+
+        # take profit
+
+        # exits
